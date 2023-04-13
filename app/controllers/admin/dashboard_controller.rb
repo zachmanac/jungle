fc5bc0b98512c88b023edde3fe94bc2183c67a35
zachmanac <zachstephenson94@gmail.com>
2023-04-13 13:58:35 -0700

    add basic admin dashboard layout

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class Admin::DashboardController < ApplicationController
+  
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
 
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
+
 end
